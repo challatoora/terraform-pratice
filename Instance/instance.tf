751fcbea6e8b28777310fa4d6a9e3a25ab3e7dcc
challatoor murali <challatoor murali.gmail.com>
2026-05-08 12:32:45 +0530

    terra

--- Instance/instance.tf
+++ Instance/instance.tf
@@ -1,18 +1,7 @@
 
 resource "aws_instance" "murali" {
   ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t2.micro"
-  count = var.ashok == "prod" ? 1 : 0
+  instance_type = var.ashok == "prod"? "t3.large":"t2.micro"
+  
 }
 
-
-resource "aws_instance" "ashok" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.micro"
-  count = var.ashok == "dev" ? 1 : 0
-
-
-  tags = {
-    Name = "cocola"
-  }
-}
