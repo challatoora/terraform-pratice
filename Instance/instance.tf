709fe6990ffc418dc826da213cdcbfea9053511c
challatoor murali <challatoor murali.gmail.com>
2026-05-08 12:12:28 +0530

    terra

--- Instance/instance.tf
+++ Instance/instance.tf
@@ -2,8 +2,17 @@
 resource "aws_instance" "murali" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t2.micro"
+  count = var.ashok == "prod" ? 1 : 0
+}
+
+
+resource "aws_instance" "ashok" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t2.micro"
+  count = var.ashok == "dev" ? 1 : 0
+
 
   tags = {
-    Name = "Terraform-Server"
+    Name = "cocola"
   }
 }
---
+++ Instance/variable.tf
@@ -0,0 +1,3 @@
+variable "ashok" {
+default = "prod"
+}
