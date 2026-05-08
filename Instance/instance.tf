590561c6c292de4f1230a911789ba2e9733328e8
challatoor murali <challatoor murali.gmail.com>
2026-05-08 12:49:41 +0530

    terra

--- Instance/instance.tf
+++ Instance/instance.tf
@@ -2,6 +2,6 @@
 resource "aws_instance" "murali" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = var.ashok == "prod"? "t3.micro":"t2.micro"
-  
+  count = var.ashok == "prod"? 2 : 1
 }
 
