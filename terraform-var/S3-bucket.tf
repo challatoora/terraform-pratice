4211193c8de23cbd0635d35ed2d2a5af634955c9
challatoor murali <challatoor murali.gmail.com>
2026-07-19 15:03:15 +0530

    tf

--- terraform-var/S3-bucket.tf
+++ terraform-var/S3-bucket.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "Tf_bucket"{
-    bucket="tf-demo-9876543210"
+# resource "aws_s3_bucket" "Tf_bucket"{
+#     bucket="tf-demo-9876543210"
 
-    tags = {
-    Name        = "My bucket"
-    #Environment = "Dev"
-  }
-}
+#     tags = {
+#     Name        = "My bucket"
+#     #Environment = "Dev"
+#   }
+# }
